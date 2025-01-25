9b3d1e40b6889ef064949750355f7ce5a58e8bb2
Oskar Oberg <contact@oskaroberg.com>
2025-01-25 21:14:54 +0100

    feat: Add install script

---
+++ dotfiles/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ -z "$HOME" ]; then echo "Seems you're \$HOMEless :("; exit 1; fi
+
+DOTCONFIG=$HOME/.config
+DOTFILES=$HOME/.arch-config/dotfiles
+
+GITCLONE="git clone --depth=1"
+
+cd "$HOME" || exit
+rm -rf "$DOTFILES"
+mkdir "$DOTFILES"
+cd "$DOTFILES" || exit
+
+git clone git@github.com:OB3RG/arch-config.git $DOTFILES
+
+rm -rf \
+  "$HOME/.gitconfig" \
+  "$HOME/.zshrc" \
+  "$DOTCONFIG/nvim" \
+  "$DOTCONFIG/alacritty" \
+  "$DOTCONFIG/i3status" \
+  "$DOTCONFIG/rofi" \
+  "$DOTCONFIG/sway" 
+
+ln -s "$DOTFILES/gitconfig" "$HOME/.gitconfig"
+ln -s "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+ln -s "$DOTFILES/config/nvim" "$DOTCONFIG/nvim"
+ln -s "$DOTFILES/config/alacritty" "$DOTCONFIG/alacritty"
+ln -s "$DOTFILES/config/i3status" "$DOTCONFIG/i3status"
+ln -s "$DOTFILES/config/rofi" "$DOTCONFIG/rofi"
+ln -s "$DOTFILES/config/sway" "$DOTCONFIG/sway"
+
+cd "$HOME" || exit
+rm -f "${HOME}/.zcompdump*"
+
+echo "ENJOY! :)"
