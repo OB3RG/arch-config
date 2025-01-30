746536c21e4e23d7887b7d9c4fb74bbbd92fec91
Oskar Öberg <oskar@shimayama.io>
2025-01-30 15:10:30 +0100

    feat: Add more packages to misc

--- installation/misc.sh
+++ installation/misc.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Sy
 
-sudo pacman -S --noconfirm zsh zsh-autosuggestions zsh-syntax-highlighting fzf gnome-keyring seahorse obsidian syncthing
+sudo pacman -S --noconfirm zsh zsh-autosuggestions zsh-syntax-highlighting fzf gnome-keyring seahorse obsidian syncthing ripgrep steam ttf-liberation
 
 sleep 5
 echo "Reboot"
