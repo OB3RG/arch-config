2034ca81f8d9e67574c3a979e23df333bedafe36
Oskar Öberg <oskar@shimayama.io>
2025-03-14 13:15:09 +0100

    gruvbox

--- installation/sway.sh
+++ installation/sway.sh
@@ -6,7 +6,7 @@ sudo hwclock --systohc
 sudo reflector -c Sweden -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Sy
 
-sudo pacman -S --noconfirm gdm sway swaylock swayidle swaybg foot wmenu arc-gtk-theme arc-icon-theme firefox gnu-free-fonts ttf-ubuntu-font-family rofi-wayland otf-opendyslexic-nerd i3status
+sudo pacman -S --noconfirm gdm sway swaylock swayidle swaybg foot wmenu arc-gtk-theme arc-icon-theme firefox gnu-free-fonts ttf-ubuntu-font-family rofi-wayland otf-opendyslexic-nerd i3status gtk-engine-murrine
 
 sudo systemctl enable gdm
 sleep 5
